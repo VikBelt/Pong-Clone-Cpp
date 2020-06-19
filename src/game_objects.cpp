497b548f932fc5fdf469fb414dc24d1d360e16c0
Vikram Belthur <46857503+VikBelt@users.noreply.github.com>
2020-06-19 00:20:08 -0400

    embed the class in a namespace

--- src/game_objects.cpp
+++ src/game_objects.cpp
@@ -1,79 +1,94 @@
+/*
+Class Method Declarations
+Created by Vikram Belthur 6.18.2020
+*/
+
 #include <iostream>
 #include "game_objects.h"
 
-//Class - Vector
-Vector::Vector(double xpos = 0.0, double ypos = 0.0) : x{ xpos }, y{ ypos } {
-	//Empty Body
-}
+namespace Pong {
 
-Vector Vector::operator*(double scale) {
-	return Vector(x * scale, y * scale);
-}
+	//Class - Vector
+	Vector::Vector(double xpos = 0.0, double ypos = 0.0) : x{ xpos }, y{ ypos } {
+		//Empty Body
+	}
 
-Vector Vector::operator+(Vector source) {
-	return Vector(x + source.x, y + source.y);
-}
+	Vector Vector::operator*(double scale) {
+		return Vector(x * scale, y * scale);
+	}
 
-Vector& Vector::operator+=(const Vector& source) {
-	x = x + source.x;
-	y = y + source.y;
-	return *this;
-}
+	Vector Vector::operator+(Vector source) {
+		return Vector(x + source.x, y + source.y);
+	}
 
-//Class - Ball
-Ball::Ball(Vector s_position) {
-	//want to define the Ball rectangle
-	position = s_position;
-	ball.x = (int)position.x;
-	ball.y = (int)position.y;
-	ball.w = BALL_WIDTH;
-	ball.h = BALL_HEIGHT;
-}
+	Vector& Vector::operator+=(const Vector& source) {
+		x = x + source.x;
+		y = y + source.y;
+		return *this;
+	}
 
-void Ball::Show(SDL_Renderer* rend) {
-	//want to display the rectangle from the constructor
-	ball.x = (int)position.x;
-	ball.y = (int)position.y;
-	SDL_RenderFillRect(rend, &ball);
-}
+	//Class - Ball
+	Ball::Ball(Vector s_position) {
+		//want to define the Ball rectangle
+		position = s_position;
+		ball.x = (int)position.x;
+		ball.y = (int)position.y;
+		ball.w = BALL_WIDTH;
+		ball.h = BALL_HEIGHT;
+	}
 
-//Class - Paddle
-Paddle::Paddle(Vector s_position) {
-	//want to define the Ball rectangle
-	position = s_position;
-	paddle.x = (int)position.x;
-	paddle.y = (int)position.y;
-	paddle.w = PADDLE_WIDTH;
-	paddle.h = PADDLE_HEIGHT;
-}
+	void Ball::Show(SDL_Renderer* rend) {
+		//want to display the rectangle from the constructor
+		ball.x = (int)position.x;
+		ball.y = (int)position.y;
+		SDL_RenderFillRect(rend, &ball);
+	}
 
-void Paddle::Show(SDL_Renderer* rend) {
-	//want to display the rectangle from the constructor
-	paddle.x = (int)position.x;
-	paddle.y = (int)position.y;
-	SDL_RenderFillRect(rend, &paddle);
-}
+	//Class - Paddle
+	Paddle::Paddle(Vector s_position, Vector s_velocity) : position{ s_position }, velocity{ s_velocity } {
+		//want to define the Ball rectangle
+		paddle.x = (int)position.x;
+		paddle.y = (int)position.y;
+		paddle.w = PADDLE_WIDTH;
+		paddle.h = PADDLE_HEIGHT;
+	}
 
-//Class - Score
-Score::Score(Vector score_position, SDL_Renderer* renderer, TTF_Font* font_data) : rend{ renderer }, gfont{ font_data } 
-{
-	position = score_position;
-	surf = TTF_RenderText_Solid(gfont, "0", { 0xFF, 0xFF, 0xFF, 0xFF });
-	texture = SDL_CreateTextureFromSurface(renderer, surf);
-	int width{};
-	int height{};
-	SDL_QueryTexture(texture, nullptr, nullptr, &width, &height);
-	score_box.x = (int)position.x;
-	score_box.y = (int)position.y;
-	score_box.w = width;
-	score_box.h = height;
-}
+	void Paddle::Show(SDL_Renderer* rend) {
+		//want to display the rectangle from the constructor
+		paddle.x = (int)position.x;
+		paddle.y = (int)position.y;
+		SDL_RenderFillRect(rend, &paddle);
+	}
 
-Score::~Score() {
-	SDL_FreeSurface(surf);
-	SDL_DestroyTexture(texture);
-}
+	void Paddle::Update(double time) {
+		//want to implement speed = distance * time
+		position = position * time;
+		//want to set bounds on how high the paddle can be
+		if (position.y < 0) { position.y = 0; }
+		else if (position.y > GAME_HEIGHT - PADDLE_HEIGHT) { position.y = GAME_HEIGHT - PADDLE_HEIGHT; }
+	}
 
-void Score::Show() {
-	SDL_RenderCopy(rend, texture, nullptr, &score_box);
-}
+	//Class - Score
+	Score::Score(Vector score_position, SDL_Renderer* renderer, TTF_Font* font_data) : rend{ renderer }, gfont{ font_data } {
+		position = score_position;
+		surf = TTF_RenderText_Solid(gfont, "0", { 0xFF, 0xFF, 0xFF, 0xFF });
+		texture = SDL_CreateTextureFromSurface(renderer, surf);
+		int width{};
+		int height{};
+		SDL_QueryTexture(texture, nullptr, nullptr, &width, &height);
+		score_box.x = (int)position.x;
+		score_box.y = (int)position.y;
+		score_box.w = width;
+		score_box.h = height;
+	}
+
+	Score::~Score() {
+		SDL_FreeSurface(surf);
+		SDL_DestroyTexture(texture);
+	}
+
+	void Score::Show() {
+		SDL_RenderCopy(rend, texture, nullptr, &score_box);
+	}
+
+} //namespace Pong
