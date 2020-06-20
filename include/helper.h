4b1a4524c0dad7e0ea913964680cabb0bf11174c
Vikram Belthur <46857503+VikBelt@users.noreply.github.com>
2020-06-19 23:08:55 -0400

    Update helper.h

--- include/helper.h
+++ include/helper.h
@@ -1,18 +1,31 @@
 /* 
-Created by Vikram Belthue 6.18.2020
+Created by Vikram Belthur 6.18.2020
 I am making this file to add some cleanliness to the main file
 */
 
 #pragma once
-#include <SDL.h>
-#include <SDL_ttf.h>
+#include "game_objects.h"
 
-void deleteGame(SDL_Renderer* renderer, SDL_Window* window, TTF_Font* scoreData){
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	TTF_CloseFont(scoreData);
-	TTF_Quit();
-	SDL_Quit();
-}
+namespace Pong {
+	
+	//Keys to Move Paddle
+	enum gameKeys {
+		paddleOneUp = 0,
+		paddleOneDown,
+		paddleTwoUp,
+		paddleTwoDown
+	};
+
+	//deleteGame
+	void deleteGame(SDL_Renderer* renderer, SDL_Window* window, TTF_Font* scoreData) {
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		TTF_CloseFont(scoreData);
+		TTF_Quit();
+		SDL_Quit();
+	}
+
+} //namespace Pong
+
 
 
