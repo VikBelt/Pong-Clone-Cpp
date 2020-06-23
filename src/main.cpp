c3f56982a43476f30418e8ffdd5e7cee7459e130
Vikram Belthur <46857503+VikBelt@users.noreply.github.com>
2020-06-23 16:56:33 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 
-//Created by Vikram Belthur 6.21.2020
+//Created by Vikram Belthur
 //Code to Test the pong Game
 
+#include "game_int.h"
 #include "game.h"
 
 int main() {
 	//runs the pong-clone
+	if (!StartMenu()) {
+		return 1;
+	}
+
 	RunGame();
 	return 0;
 }
-
